16a1e2046a1cafaf3f94a4bc3d85e0630e51de03
Bryan Cattle <bryan.cattle@gmail.com>
2017-12-06 12:01:02 -0500

    Initial Commit

---
+++ cryptowatch-swift/cryptowatch_swift.h
@@ -0,0 +1,19 @@
+//
+//  cryptowatch_swift.h
+//  cryptowatch-swift
+//
+//  Created by Bryan on 12/6/17.
+//  Copyright © 2017 com.github.bcattle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cryptowatch_swift.
+FOUNDATION_EXPORT double cryptowatch_swiftVersionNumber;
+
+//! Project version string for cryptowatch_swift.
+FOUNDATION_EXPORT const unsigned char cryptowatch_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cryptowatch_swift/PublicHeader.h>
+
+
